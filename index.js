cfc6a09787d42594a762d121fd0b059b4e762338
Paul Paniagua <pauljosepq2@gmail.com>
2022-05-17 21:04:51 -0400

    [ADD] User input library

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const prompt = require('prompt-sync')
+const prompt = require('prompt-sync')()
 
-const ssn = prompt("Introduce your social security number");
+const ssn = prompt("Introduce a Social Security Number: ");
 const regex = /((?!000|666)[0-8]\d{2})-((?!00)\d{2})-((?!0000)\d{4})/g
 
 if (regex.test(ssn)) {
